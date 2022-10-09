080c71810d987a83e7b3bba8f508c35ea9f52770
AlprT <112287824+AlprT@users.noreply.github.com>
2022-10-09 22:01:40 +0300

    Add files via upload

---
+++ CS-Examples/Smallest_Can_Be_Divided_By_1-20/Smallest_Can_Be_Divided_By_1-20.cs
@@ -0,0 +1,31 @@
+﻿namespace dnm
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int res = 20;
+
+            while(!Control(res))
+            {
+                res++;
+
+            }
+
+            Console.WriteLine(res);
+        }
+        public static bool Control(int num)
+        {
+            int i = 1;
+
+            while (i <= 20)
+            {
+                if (num % i == 0)
+                    i++;
+                else
+                    return false;
+            }
+            return true;
+        }
+    }
+}
